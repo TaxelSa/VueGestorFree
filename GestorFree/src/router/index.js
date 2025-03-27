e46fb14b2e8d6bfc5630744c7c3d8da9341b9c78
AlanAriRod <ariasalan163@gmail.com>
2025-03-27 13:34:49 -0600

    EquipoDeNuevo
    Hola

--- GestorFree/src/router/index.js
+++ GestorFree/src/router/index.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Proyectos from '../components/proyectos.vue';
+import Equipo from '../components/Equipo.vue';
 
 const routes = [
   { path: '/', component: () => import('../components/HelloWorld.vue') },
   { path: '/proyectos', component: Proyectos }, 
+  { path: '/equipo', component: Equipo }
 ];
 
 const router = createRouter({
