0fc8e0160bb8fdb4386559fcaa3cd790cf467f90
JoAVG <alfredopooo02@gmail.com>
2025-03-27 13:45:47 -0600

    Inicio, Logo y titulo
    Se agreggo inicio y alan hecho a perder el vue

--- GestorFree/src/router/index.js
+++ GestorFree/src/router/index.js
@@ -1,19 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
-<<<<<<< Updated upstream
 import Proyectos from '../components/proyectos.vue';
 import Equipo from '../components/Equipo.vue';
-
-const routes = [
-  { path: '/', component: () => import('../components/HelloWorld.vue') },
-  { path: '/proyectos', component: Proyectos }, 
-  { path: '/equipo', component: Equipo }
-=======
 import Inicio from '../components/inicio.vue';
 
 const routes = [
   { path: '/', component: Inicio },
-  { path: '/inicio', component: Inicio }, // Importación estática
->>>>>>> Stashed changes
+  { path: '/inicio', component: Inicio }, 
+  { path: '/proyectos', component: Proyectos }, 
+  { path: '/equipo', component: Equipo }
+
 ];
 
 const router = createRouter({
